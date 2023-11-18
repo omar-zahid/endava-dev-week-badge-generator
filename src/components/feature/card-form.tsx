ec3946c657d97104ad3bd151cca87f396c91702d
Omar Zahid <omarzahid@outlook.com>
2023-11-18 16:33:51 +0800

    Registered form fields

--- src/components/feature/card-form.tsx
+++ src/components/feature/card-form.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -18,8 +19,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { FormField } from "../ui/form";
+
+type FormValues = {
+  name: string;
+  company: string;
+};
 
 export function CardWithForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
   return (
     <Card className="w-[350px]">
       <CardHeader>
@@ -29,29 +44,48 @@ export function CardWithForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form id="my-form" onSubmit={handleSubmit(onSubmit)}>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Your name on the badge" />
+              <Input
+                id="name"
+                placeholder="Your name on the badge"
+                {...register("name")}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Company</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Endava Malaysia</SelectItem>
-                  <SelectItem value="sveltekit">Endava Romania</SelectItem>
-                </SelectContent>
-              </Select>
+              <FormField
+                control={control}
+                name="company"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger id="framework">
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="Endava Malaysia">
+                        Endava Malaysia
+                      </SelectItem>
+                      <SelectItem value="Endava Romania">
+                        Endava Romania
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button className="w-full">Download</Button>
+        <Button type="submit" form="my-form" className="w-full">
+          Download
+        </Button>
       </CardFooter>
     </Card>
   );
